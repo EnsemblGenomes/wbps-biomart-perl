3c87433d71653137a219f7d6194f39ab0fff90fa
Bruce Bolt <bbolt@ebi.ac.uk>
2015-07-20 13:08:15 +0100

    Change layout of OLS (i.e. revert b558089a436113903542918dd94dd7f626c7ad9a)

--- htdocs/martview/js/10_OLS.js
+++ htdocs/martview/js/10_OLS.js
@@ -3,10 +3,10 @@
 
 $(document).ready(function() {
 	$("<style type='text/css'> .ui-autocomplete-loading{ background:url('/biomart/mview/images/jquery-ui-loading.gif') no-repeat right center }</style>").appendTo('head');
-	$("textarea[name$='filter\\.go_parent_term']").attr('placeholder', 'Enter one or more accession numbers (e.g. GO:0050789)');
-	$("input[name$='filter\\.go_parent_name']").attr('placeholder', 'Start typing description to lookup GO term...')
-		.css('width', '300px')
-		.autocomplete({
+	$("div[id$='filter\\.go_parent_term']").prepend('<input type="text" id="ols-autocomplete" name="ols-autocomplete" placeholder="Start typing description to lookup GO term..." style="width: 300px; margin-bottom: 15px" /><br />');
+	$("textarea[name$='filter\\.go_parent_term']").css('width', '300px');
+	$("textarea[name$='filter\\.go_parent_term']").attr('placeholder', 'Search for a GO term above to populate this list');
+	$("#ols-autocomplete").autocomplete({
 		source: function(request, response) {
 			var endPoint;
 			var requestData;
@@ -36,7 +36,7 @@ $(document).ready(function() {
 							var displayVal = $('name', this).text() + ' [' + $('value', this).text() + ']';
 							return {
 								value: displayVal,
-								id: $('name', this).text(),
+								id: $('value', this).text(),
 							};
 						}).get();
 					} else {
@@ -56,9 +56,10 @@ $(document).ready(function() {
 		},
 		minLength: 3,
 		select: function(event, ui) {
-			var box = $("input[name$='filter\\.go_parent_name']");
-			box.val(ui.item.id);
-			$("input[name$='filtercollection\\.go_filters_name']").prop('checked', true);
+			var box = $("textarea[name$='filter\\.go_parent_term']");
+			box.val(box.val() + ui.item.id + '\n');
+			$('#ols-autocomplete').val('');
+			$("input[name$='filtercollection\\.go_filters']").prop('checked', true);
 			return false;
 		},
 	});
