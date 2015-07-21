c107e5adde6582ef66a4c88583f8b539cd974b39
Bruce Bolt <bbolt@ebi.ac.uk>
2015-07-21 10:12:27 +0100

    Disable counts for GO terms

--- htdocs/martview/js/10_OLS.js
+++ htdocs/martview/js/10_OLS.js
@@ -6,6 +6,27 @@ $(document).ready(function() {
 	$("div[id$='filter\\.go_parent_term']").prepend('<input type="text" id="ols-autocomplete" name="ols-autocomplete" placeholder="Start typing description to lookup GO term..." style="width: 300px; margin-bottom: 15px" /><br />');
 	$("textarea[name$='filter\\.go_parent_term']").css('width', '300px');
 	$("textarea[name$='filter\\.go_parent_term']").attr('placeholder', 'Search for a GO term above to populate this list');
+	$("input[name$='filtercollection\\.go_filters']").change( function() {
+		if($(this).is(':checked')) {
+			$('#count_button_tag')
+				.removeAttr('onclick')
+				.removeAttr('onmouseover')
+				.removeAttr('onmouseout')
+				.removeClass('mart_btn_count')
+				.addClass('mart_btn_count_disabled')
+				.click( function() {
+					alert('Due to the complexity of this query, a count is not possible.  The first ten results can be previewed by clicking "Results".');
+				});
+		} else {
+			$('#count_button_tag')
+				.unbind('click')
+				.attr('onclick', "if (datasetExists() > 0) { document.getElementById('summarypanel_filter_count_1').innerHTML = 'Loading... '; addLoadingImage('summarypanel_filter_count_1', 'biomart');  if (document.getElementById('summarypanel_filter_count_2') != null){  document.getElementById('summarypanel_filter_count_2').innerHTML = 'Loading... ';  addLoadingImage('summarypanel_filter_count_2', 'biomart'); } document.mainform.formatterMenu.value = 'preView'; document.mainform.do_export.value = 0; document.mainform.track_visible_section.value = document.mainform['mart_mainpanel__current_visible_section'].value; document.mainform.savequery.value = 0; document.mainform.showquery.value = 0; document.mainform.countButton.value = 5; document.mainform.target = 'count_hiddenIFrame'; document.mainform.submit();  document.mainform.countButton.value = 1;   return false; }")
+				.attr('onmouseover', "this.className='mart_btn_count mart_btnhov'")
+				.attr('onmouseout', "this.className='mart_btn_count'")
+				.addClass('mart_btn_count')
+				.removeClass('mart_btn_count_disabled');
+		}
+	})
 	$("#ols-autocomplete").autocomplete({
 		source: function(request, response) {
 			var endPoint;
@@ -59,7 +80,7 @@ $(document).ready(function() {
 			var box = $("textarea[name$='filter\\.go_parent_term']");
 			box.val(box.val() + ui.item.id + '\n');
 			$('#ols-autocomplete').val('');
-			$("input[name$='filtercollection\\.go_filters']").prop('checked', true);
+			$("input[name$='filtercollection\\.go_filters']").prop('checked', true).trigger('change');
 			return false;
 		},
 	});
