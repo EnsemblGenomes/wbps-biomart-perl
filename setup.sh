67bcd1c5860c4081520c7e986c31362ad713cb8a
Tuan Le <tuanle@ebi.ac.uk>
2023-03-29 09:37:40 +0000

    Update setup script to include staging rest

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Script to setup ParaSite $LIVE_PATH and archive biomart, usage: ./setup.sh [--archive | --fallback]
-
+# Script to setup ParaSite staging/live/fallback/archive biomart, usage: ./setup.sh [--archive | --fallback | --staging1 | --staging2]
+set -e
 LIVE_PATH='live'
 DATABASE_HOST='pg-mysql-ps-mart-rel'
 DATABASE_PORT='4474'
@@ -13,6 +13,18 @@ fi
 
 if [ "$1" = "--fallback" ]; then
    DATABASE_HOST='fb1-mysql-ps-mart-rel'
+fi
+
+if [ "$1" = "--staging1" ]; then
+   LIVE_PATH='staging'
+   DATABASE_HOST='mysql-ps-staging-1'
+   DATABASE_PORT='4451'
+fi
+
+if [ "$1" = "--staging2" ]; then
+   LIVE_PATH='staging'
+   DATABASE_HOST='mysql-ps-staging-2'
+   DATABASE_PORT='4467'
 fi
 
 if [ -z "$RELEASE_VERSION" ]; then
@@ -41,6 +53,7 @@ cp -r /nfs/public/release/ensweb/$LIVE_PATH/parasite-mart/release-$RELEASE_VERSI
 echo "Creating databases connection at /nfs/incoming/ensweb/$LIVE_PATH/parasite-mart/release-$RELEASE_VERSION/conf/registry.xml"
 sed -e "s/#PSVERSION#/$RELEASE_VERSION/; s/#ENSVERSION#/$ENS_VERSION/; s/#DATABASE_HOST#/$DATABASE_HOST/; s/#DATABASE_PORT#/$DATABASE_PORT/" /nfs/incoming/ensweb/$LIVE_PATH/parasite-mart/release-$RELEASE_VERSION/conf/registryURLPointer.xml > /nfs/incoming/ensweb/$LIVE_PATH/parasite-mart/release-$RELEASE_VERSION/conf/registry.xml
 sed -i "s/<release_version>/release-$RELEASE_VERSION/" /nfs/incoming/ensweb/$LIVE_PATH/parasite-mart/release-$RELEASE_VERSION/conf/settings.conf
+sed -i "s/<stage_path>/$LIVE_PATH/" /nfs/incoming/ensweb/$LIVE_PATH/parasite-mart/release-$RELEASE_VERSION/conf/settings.conf
 cd /nfs/public/release/ensweb/$LIVE_PATH/parasite-mart/release-$RELEASE_VERSION/biomart/
 export PERL5LIB=.:$PERL5LIB
 ./redo.sh "/nfs/incoming/ensweb/$LIVE_PATH/parasite-mart/release-$RELEASE_VERSION/conf/"
